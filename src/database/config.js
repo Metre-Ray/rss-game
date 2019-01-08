af57d050fbc95f7ea61f7d03a98203520e9377ad
Metre-Ray <dgary@tut.by>
2019-01-08 22:42:08 +0300

    feat: add all files

---
+++ src/database/config.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyByePZr8lxK4jODJ0-RCwNZrbxhul2nzuM",
+    authDomain: "apprsgame.firebaseapp.com",
+    databaseURL: "https://apprsgame.firebaseio.com",
+    projectId: "apprsgame",
+    storageBucket: "apprsgame.appspot.com",
+    messagingSenderId: "458449210306"
+};
+
+firebase.initializeApp(config);
